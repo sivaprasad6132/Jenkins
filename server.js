a6cd9c6e8061ef4e3b241db0c1bd7e47c79fe415
adi siva <sivaprasad6132@gmail.com>
2025-02-24 17:20:09 -0800

    adding s

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
             <label>Course Goal</label>
             <input type="text" name="goal">
           </div>
-          <button>Set Course Goalsssss</button>
+          <button>Set Course Goalssss</button>
         </form>
       </body>
     </html>
